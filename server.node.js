c6ee027788d2a540704462f87fa4d5c2d959d25e
Don Undeen <don.undeen@metmuseum.org>
2014-10-13 20:49:18 +0000

    got it working on a ubuntu server, plenty of jquery/jsdom problems

--- server.node.js
+++ server.node.js
@@ -1,7 +1,7 @@
 /*
 server for data-integrator
 */
-
+//var GLOBAL = {};
 
 var Percolator = require('percolator').Percolator;
 
@@ -11,8 +11,27 @@ var urlparser = require("url");
 var pathparser = require("path");
 var fs = require("fs");
 
-var jsdom = require("jsdom"); 
-$ = require("jquery")(jsdom.jsdom().createWindow()); 
+//var jsdom = require("jsdom"); 
+//$ = require("jquery");//(jsdom.jsdom().createWindow()); 
+
+
+var jsdom = require('jsdom');
+var $;
+
+jsdom.env({  
+  html: "<html><body></body></html>",
+  scripts: [
+    'http://code.jquery.com/jquery-1.5.min.js'
+  ],
+  done : function (err, window) {
+    $ = window.jQuery;
+    GLOBAL.$ = $;
+    get_server_started();
+  } 
+});
+
+
+
 
 var secrets = require("./secrets.js").secrets();
 
@@ -59,141 +78,147 @@ type: object/facedata/issmiles
 
 
 */
+var WidgetManager;
+var widgetServerSideFunctions;
+
+function get_server_started(){
 
 
 // call the widgetMananger and find out if there are any server-side functions for widgets.
-var WidgetManager = require("./classes/widget/widget.js").WidgetManager();
+   WidgetManager = require("./classes/widget/widget.js").WidgetManager();
 
-var widgetServerSideFunctions;
+   widgetServerSideFunctions;
 
-WidgetManager.registerServerSideWidgetFunctions(function(list){
-  widgetServerSideFunctions = list;
-});
-
-
-server.route(
-
-  '*', {  
-    GET : function(req, res){
-  		// get from db and return
-      var parsed = urlparser.parse(req.url, true)
-      var query = parsed.query;
+  WidgetManager.registerServerSideWidgetFunctions(function(list){
+    widgetServerSideFunctions = list;
+  });
 
 
-      // CLIENT OR server-sidE GENERATION? DECIDE HERE. HOW?
+  server.route(
 
-      console.log("@@@@@@@@@@@@@@@@@@@@@@@@ REQUEST");
-      console.log(req.url) ;
- 
-      if(req.url.match(/\.(html|js|jpg|jpeg|gif|png|css|ico|ttf|svg|woff)(\?.*)?$/i)){
-        if(!query.action){
-          // this is doing it client-side
-          sendFile(parsed.pathname, query, res);
+    '*', {  
+      GET : function(req, res){
+    		// get from db and return
+        var parsed = urlparser.parse(req.url, true)
+        var query = parsed.query;
+
+
+        // CLIENT OR server-sidE GENERATION? DECIDE HERE. HOW?
+
+        console.log("@@@@@@@@@@@@@@@@@@@@@@@@ REQUEST");
+        console.log(req.url) ;
+   
+        if(req.url.match(/\.(html|js|jpg|jpeg|gif|png|css|ico|ttf|svg|woff)(\?.*)?$/i)){
+          if(!query.action){
+            // this is doing it client-side
+            sendFile(parsed.pathname, query, res);
+            return;
+          }
+        }
+        if(req.url.match(/^\/widgetlist$/)){
+          var widgetManager = require("./classes/widget/widget.js").WidgetManager();
+          var filelist = widgetManager.getWidgetList(function(list){
+            var contentType = "application/json";
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(JSON.stringify(list));
+          });
           return;
         }
-      }
-      if(req.url.match(/^\/widgetlist$/)){
-        var widgetManager = require("./classes/widget/widget.js").WidgetManager();
+
+        if(req.url.match(/^\/proxy\//)){
+          var split = req.url.split("/");
+          split.shift(); split.shift();
+          var url = split.join("/");
+          
+          var proxy = require("./classes/proxy/proxy.js").ProxyManager();
+          var result = proxy.callUrl(url, function(data){
+            var contentType = "application/json";
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(JSON.stringify(data));
+          });
+
+          return;
+        }
+
+        // this is doign it server-side
+        if(req.url.match(/^\/couchdb\//)){
+
+          var couchid = req.url.replace(/^\/couchdb\//, "");
+          var  db = require("./classes/db/db.js").DbManager();
+          db.loadDoc(couchid, function(doc){
+            var contentType = "application/json";
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(JSON.stringify(doc));
+          });
+          return;
+        }
+
+
+        $.each(widgetServerSideFunctions.GET, function(index, theFunction){
+          console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+          console.log(req.url);
+          if(req.url.match(theFunction.match)){
+              theFunction.theFunction(req, res);
+          }
+        });
+
+        return;
+
+        var EntityManager = require(GLOBAL.params.root_dir+"/classes/entity/entity.js").EntityManager();
+
+        var entity = EntityManager.generateEntity(req.url, function(entity){
+          var contentType = "application/json";
+          res.writeHead(200, {'Content-Type': contentType});
+          res.end(JSON.stringify(entity));
+
+        });
+
+
+  //      res.object({message : 'Hello World!'}).send();
+
+      },
+      PUT : function(req, res){
+      	// creating a NEW resource, or updating it.
+        console.log("@@@@@@@@@@@@@@@@@@@@@@@@ PUT REQUEST");
+        console.log(req.url) ;
+        var contentType = "application/json";
+        req.onJson(function(err, obj){
+          if(err){
+            console.log("PUT error in json "+ err);
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(JSON.stringify({message : "insert ERROR"}));
+            return;
+          }
+          // do something with json data
+          var db = require("./classes/db/db.js").DbManager();
+          db.insertDoc(obj, function(result){
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(JSON.stringify(result));
+           
+          });
+
+        });
+       
+      },
+    },
+    '/widgetlist',  {
+      GET : function(req, res){
+        var widgetManager = require("./classes/widget/widget.js");
         var filelist = widgetManager.getWidgetList(function(list){
           var contentType = "application/json";
           res.writeHead(200, {'Content-Type': contentType});
           res.end(JSON.stringify(list));
         });
-        return;
       }
-
-      if(req.url.match(/^\/proxy\//)){
-        var split = req.url.split("/");
-        split.shift(); split.shift();
-        var url = split.join("/");
-        
-        var proxy = require("./classes/proxy/proxy.js").ProxyManager();
-        var result = proxy.callUrl(url, function(data){
-          var contentType = "application/json";
-          res.writeHead(200, {'Content-Type': contentType});
-          res.end(JSON.stringify(data));
-        });
-
-        return;
-      }
-
-      // this is doign it server-side
-      if(req.url.match(/^\/couchdb\//)){
-
-        var couchid = req.url.replace(/^\/couchdb\//, "");
-        var  db = require("./classes/db/db.js").DbManager();
-        db.loadDoc(couchid, function(doc){
-          var contentType = "application/json";
-          res.writeHead(200, {'Content-Type': contentType});
-          res.end(JSON.stringify(doc));
-        });
-        return;
-      }
-
-
-      $.each(widgetServerSideFunctions.GET, function(index, theFunction){
-        console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-        console.log(req.url);
-        if(req.url.match(theFunction.match)){
-            theFunction.theFunction(req, res);
-        }
-      });
-
-      return;
-
-      var EntityManager = require(GLOBAL.params.root_dir+"/classes/entity/entity.js").EntityManager();
-
-      var entity = EntityManager.generateEntity(req.url, function(entity){
-        var contentType = "application/json";
-        res.writeHead(200, {'Content-Type': contentType});
-        res.end(JSON.stringify(entity));
-
-      });
-
-
-//      res.object({message : 'Hello World!'}).send();
-
-    },
-    PUT : function(req, res){
-    	// creating a NEW resource, or updating it.
-      console.log("@@@@@@@@@@@@@@@@@@@@@@@@ PUT REQUEST");
-      console.log(req.url) ;
-      var contentType = "application/json";
-      req.onJson(function(err, obj){
-        if(err){
-          console.log("PUT error in json "+ err);
-          res.writeHead(200, {'Content-Type': contentType});
-          res.end(JSON.stringify({message : "insert ERROR"}));
-          return;
-        }
-        // do something with json data
-        var db = require("./classes/db/db.js").DbManager();
-        db.insertDoc(obj, function(result){
-          res.writeHead(200, {'Content-Type': contentType});
-          res.end(JSON.stringify(result));
-         
-        });
-
-      });
-     
-    },
-  },
-  '/widgetlist',  {
-    GET : function(req, res){
-      var widgetManager = require("./classes/widget/widget.js");
-      var filelist = widgetManager.getWidgetList(function(list){
-        var contentType = "application/json";
-        res.writeHead(200, {'Content-Type': contentType});
-        res.end(JSON.stringify(list));
-      });
     }
-  }
 
-);
+  );
 
-server.listen(function(err){
-  console.log('server is listening on port ', server.port);
-});
+  server.listen(function(err){
+    console.log('server is listening on port ', server.port);
+  });
+}
+
 
 var dataCache = {};
 function sendFile(path, query, res){
