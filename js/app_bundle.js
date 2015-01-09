3d772f3d340d178f0294d838a9f4c6941b5b0b39
don undeen <don.undeen@metmuseum.org>
2015-01-08 23:48:46 -0500

    got the image recognizer widget working
    layout is really bad, but that will probably wait until I’ve refactored
    the html/css stuff.

--- js/app_bundle.js
+++ js/app_bundle.js
@@ -62279,19 +62279,32 @@ function OpenCVWidget(){
 			if(this.data.parsedUrl){
 				GLOBAL.$(urlcontent).text(this.data.parsedUrl);
 			}
-			var jsoncontent = GLOBAL.$('<div class="panel-group" id="accordionJSON'+this.widget.uniqueName+'"><div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordionJSON'+this.widget.uniqueName+'" href="#collapseOneJSON'+this.widget.uniqueName+'">JSON 	</a><span class="loadingindicator"/></h4></div><div id="collapseOneJSON'+this.widget.uniqueName+'" class="panel-collapse collapse"><div class="panel-body"><pre/></div></div></div>');
+			var imagecontent = GLOBAL.$('<div class="panel-group" id="accordionJSON'+this.widget.uniqueName+'">'+
+											'<div class="panel panel-default"><div class="panel-heading">'+
+											'<h4 class="panel-title">'+
+											'<a data-toggle="collapse" data-parent="#accordionJSON'+this.widget.uniqueName+'" href="#collapseOneJSON'+this.widget.uniqueName+'">JSON 	</a><span class="loadingindicator"/></h4></div>'+
+											'<div id="'+realthis.widget.uniqueName+'_imageholder" class="imageholder">imageholder</div>'+
+											'<div class="controls">controls</div></div>');
 			container.append(urlcontent);
-			container.append(jsoncontent);
-			GLOBAL.$(".loadingindicator", jsoncontent).text("loading...");
+			container.append(imagecontent);
+			GLOBAL.$(".loadingindicator", imagecontent).text("loading...");
+			this.setupCanvas(realthis.widget.uniqueName+'_imageholder', function(paper){
+				console.log("got paper");
+				realthis.setImageInPaper(realthis.data.parsedUrl, paper, function(queueobject, _paper, result){
+					console.log("image set in paper");
+				});
+			});
 			this.addListener("dataUpdated", function(params){
 				var url = realthis.data.parsedUrl;
 				GLOBAL.$(urlcontent).text(url);
 				if(realthis.data.json){
-					GLOBAL.$(".loadingindicator", jsoncontent).text("loaded");
-					GLOBAL.$("pre",jsoncontent).text(JSON.stringify(realthis.data.json, null, 4));
+					GLOBAL.$(".loadingindicator", imagecontent).text("loaded");
+					realthis.populateImageHolder(realthis.data.json, imagecontent);
 				}
 			});
 		}
+
+
 
 
 		widgetInstance.run = function(){
@@ -62333,6 +62346,159 @@ function OpenCVWidget(){
 //			this.run();
 		//	widgetInstance.data.random = Math.random();
 		};
+
+
+
+		widgetInstance.populateImageHolder = function(data, container){
+			console.log("populating");
+
+			this.placeIdentifiers(data, container, this.paper, this.paper.scaleFactor);
+
+		};
+
+
+
+		widgetInstance.setupCanvas = function(id, callback){
+			var realthis = this;
+			Raphael(id, 400, 600, function(context){
+				paper = this;
+				realthis.paper = paper;
+				callback(paper);
+			});
+		};
+
+
+		widgetInstance.setImageInPaper = function(url, _paper, callback){
+			_paper.clear();
+			var realthis = this;
+			queueObject = {};
+
+			data = {image: url};
+			queueObject.data= data;
+
+			var theimage= data.image;
+
+			queueObject.image = theimage;
+			queueObject.imageObj = new Image();
+			queueObject.imageObj.onload = function(){
+				queueObject.origWidth = queueObject.width;
+				queueObject.origHeight = queueObject.height;
+
+				var result = realthis.ScaleImage(queueObject.imageObj.width, queueObject.imageObj.height, 400, 600, true);
+				queueObject.paperimage = _paper.image(queueObject.image, 0,0, result.width, result.height);
+				queueObject.width = result.width;
+				queueObject.height = result.height;
+				_paper.scaleFactor = result;
+				callback(queueObject, _paper, result);
+
+			}
+			queueObject.imageObj.src = queueObject.image;
+
+		};
+
+		widgetInstance.ScaleImage = function(srcwidth, srcheight, targetwidth, targetheight, fLetterBox) {
+
+			var result = { width: 0, height: 0, fScaleToTargetWidth: true };
+
+			if ((srcwidth <= 0) || (srcheight <= 0) || (targetwidth <= 0) || (targetheight <= 0)) {
+			    return result;
+			}
+
+			// scale to the target width
+			var scaleX1 = targetwidth;
+			var scaleY1 = (srcheight * targetwidth) / srcwidth;
+
+			// scale to the target height
+			var scaleX2 = (srcwidth * targetheight) / srcheight;
+			var scaleY2 = targetheight;
+
+			var scaleX;
+			var scaleY;
+
+			// now figure out which one we should use
+			var fScaleOnWidth = (scaleX2 > targetwidth);
+			if (fScaleOnWidth) {
+			    fScaleOnWidth = fLetterBox;
+			}
+			else {
+			   fScaleOnWidth = !fLetterBox;
+			}
+
+			if (fScaleOnWidth) {
+			    result.width = Math.floor(scaleX1);
+			    result.height = Math.floor(scaleY1);
+			    result.fScaleToTargetWidth = true;
+			}
+			else {
+			    result.width = Math.floor(scaleX2);
+			    result.height = Math.floor(scaleY2);
+			    result.fScaleToTargetWidth = false;
+			}
+			result.scaleX = result.width / srcwidth;
+			result.scaleY = result.height / srcheight;
+			result.targetleft = Math.floor((targetwidth - result.width) / 2);
+			result.targettop = Math.floor((targetheight - result.height) / 2);
+
+			return result;
+		};
+
+
+
+		widgetInstance.placeIdentifiers = function(data, container, _paper, scaleFactor){
+			GLOBAL.$(".controls", container ).empty();
+			console.log("placing identifiers");
+			var _scaleFactor = scaleFactor;
+			for(key in data){
+				console.log(key);
+				values = data[key];
+				num_matches = values.length;
+				var idname = this.widget.uniqueName+"_controlspan" + key;
+				var checkid = this.widget.uniqueName+"_controlcheck" + key;
+				if(num_matches > 0){
+					var control = GLOBAL.$("<span id='"+idname+"' class='control'>"+key+" ("+num_matches+")</span>");
+					var controlCheck = GLOBAL.$("<input id='"+checkid+"' type='checkbox' />");
+					console.log("appending");
+					GLOBAL.$(".controls", container).append(controlCheck).append(control).append("<BR>");
+				}else{
+					console.log("not appending");
+				}
+				if(key != "_links"){
+					for(key2 in values){
+						value = values[key2];
+						var x = value.x * _scaleFactor.scaleX;
+						var y = value.y * _scaleFactor.scaleY;
+						var w = value.w * _scaleFactor.scaleX;
+						var h = value.h * _scaleFactor.scaleY;
+						//var rect = _paper.rect(value.x, value.y, value.w, value.h);
+						var rect = _paper.rect(x, y, w, h);
+
+						rect.attr("stroke","#f00");
+						rect.attr("stroke-width",1);
+						rect.attr("fill", "#5f5");
+						rect.attr("fill-opacity",".2");
+						rect.toFront();
+						rect.show();
+						(function(_rectid, thepaper, _checkid){
+							console.log("in anon");
+							GLOBAL.$(document).on("change", "#"+_checkid, function(){
+								console.log("clicked" + _rectid);
+								var _rect= thepaper.getById(_rectid);
+								console.log(_rect);
+								if(GLOBAL.$("#"+_checkid).is(':checked') ){
+									console.log("showing");
+									_rect.show();
+									_rect.toFront();
+								}else{
+									console.log("hiding");
+									_rect.hide();
+								}
+							});
+						})(rect.id, _paper, checkid);
+					}
+				}
+			}
+		};
+
 
 
 
